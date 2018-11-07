d8a6267fbeb7a56a10e020388d47e6840b6dcdfd
wangyihuai <wangyihuai1997@foxmail.com>
2018-11-07 17:55:45 +0800

    初始化项目

---
+++ micro-service/src/main/java/cn/com/pism/pismos/mdm/MicroServiceApplication.java
@@ -0,0 +1,12 @@
+package cn.com.pism.pismos.mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceApplication.class, args);
+    }
+}
