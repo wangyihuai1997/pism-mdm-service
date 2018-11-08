3fb60e91353f6bca059894b587c4e89a404f8744
wangyihuai <wangyihuai1997@foxmail.com>
2018-11-08 10:16:46 +0800

    初始化项目

--- micro-service/src/main/java/cn/com/pism/pismos/mdm/MicroServiceApplication.java
+++ micro-service/src/main/java/cn/com/pism/pismos/mdm/MicroServiceApplication.java
@@ -2,8 +2,12 @@ package cn.com.pism.pismos.mdm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class MicroServiceApplication {
 
     public static void main(String[] args) {
